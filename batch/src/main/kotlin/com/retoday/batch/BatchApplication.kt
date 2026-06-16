cbb16e94fd12ecab3b63e794e27751aeb9317800
mjk25 <winsomjk@gmail.com>
2026-06-16 10:03:30 +0900

    feat: 각 타임존 00시 자동 실행 및 스케줄러 on/off 옵션 추가

--- batch/src/main/kotlin/com/retoday/batch/BatchApplication.kt
+++ batch/src/main/kotlin/com/retoday/batch/BatchApplication.kt
@@ -3,7 +3,9 @@ package com.retoday.batch
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan
 import org.springframework.boot.runApplication
+import org.springframework.scheduling.annotation.EnableScheduling
 
+@EnableScheduling
 @ConfigurationPropertiesScan(basePackages = ["com.retoday.core", "com.retoday.batch"])
 @SpringBootApplication(scanBasePackages = ["com.retoday.batch", "com.retoday.core"])
 class BatchApplication
