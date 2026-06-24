0dc77d5930de2e60da010089a2c74d38d40f955b
Yelim Lee <142207312+1117mg@users.noreply.github.com>
2026-06-24 18:07:02 +0900

    [#58] 웹사이트 신규 등록 판별 방식 개선 (#59)

--- core/src/main/kotlin/com/retoday/core/domain/history/repository/CustomWebsiteRepository.kt
+++ core/src/main/kotlin/com/retoday/core/domain/history/repository/CustomWebsiteRepository.kt
@@ -3,5 +3,10 @@ package com.retoday.core.domain.history.repository
 import com.retoday.core.domain.history.entity.Website
 
 interface CustomWebsiteRepository {
-    fun upsertByDomain(website: Website): Int
+    /**
+     * 도메인 기준으로 웹사이트를 upsert하고, 새로 등록되었는지 여부를 반환한다.
+     *
+     * @return 신규 INSERT면 true, 이미 존재하던 웹사이트면 false
+     */
+    fun upsertByDomain(website: Website): Boolean
 }
