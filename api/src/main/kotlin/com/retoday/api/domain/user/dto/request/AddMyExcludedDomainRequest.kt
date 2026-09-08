5132c7696741aa7c38911b07481a02cd0652536c
earlgrey02 <san06036@naver.com>
2026-09-08 23:48:59 +0900

    fix: 예외 도메인 빈 값 검증 복구

--- api/src/main/kotlin/com/retoday/api/domain/user/dto/request/AddMyExcludedDomainRequest.kt
+++ api/src/main/kotlin/com/retoday/api/domain/user/dto/request/AddMyExcludedDomainRequest.kt
@@ -2,8 +2,10 @@ package com.retoday.api.domain.user.dto.request
 
 import com.retoday.api.global.validation.Domain
 import com.retoday.core.domain.user.dto.command.AddMyExcludedDomainCommand
+import jakarta.validation.constraints.NotBlank
 
 data class AddMyExcludedDomainRequest(
+    @field:NotBlank
     @field:Domain
     val domain: String
 ) {
