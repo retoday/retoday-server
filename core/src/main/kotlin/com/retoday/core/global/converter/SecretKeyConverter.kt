bf6cd0076d0ed688009a0117e34cf197dac72ed6
earlgrey02 <san06036@naver.com>
2026-09-14 19:41:14 +0900

    fix: 누락된 어노테이션 추가

--- core/src/main/kotlin/com/retoday/core/global/converter/SecretKeyConverter.kt
+++ core/src/main/kotlin/com/retoday/core/global/converter/SecretKeyConverter.kt
@@ -1,10 +1,14 @@
 package com.retoday.core.global.converter
 
 import io.jsonwebtoken.security.Keys
+import org.springframework.boot.context.properties.ConfigurationPropertiesBinding
 import org.springframework.core.convert.converter.Converter
+import org.springframework.stereotype.Component
 import java.util.*
 import javax.crypto.SecretKey
 
+@Component
+@ConfigurationPropertiesBinding
 class SecretKeyConverter : Converter<String, SecretKey> {
     override fun convert(source: String): SecretKey =
         Base64
